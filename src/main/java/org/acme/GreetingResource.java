6b5bcf4834c3f947238917ba0c8e1e7b1ad438dd
Pierre Taillard <pierre_taillard@yahoo.fr>
2023-01-26 16:59:20 +0000

    Add Person entity

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,7 +1,10 @@
 package org.acme;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,5 +15,19 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("persons")
+    public List<Person> persons() {
+        return Person.listAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("persons/{id}")
+    public Person person(@PathParam("id") Long id) {
+        return Person.findById(id);
     }
 }
--- src/main/java/org/acme/Person.java
+++ src/main/java/org/acme/Person.java
@@ -24,6 +24,7 @@ import javax.persistence.Entity;
  * }
  */
 @Entity
-public class MyEntity extends PanacheEntity {
-    public String field;
+public class Person extends PanacheEntity {
+    public String name;
+    public String adr;
 }
